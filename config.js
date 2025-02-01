9881125da730fc7f182cebb786700b9ba806af76
Carterefe <abdenniz1@gmail.com>
2025-02-01 01:12:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vakp0UnICVfe
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vakp0UnICVfe3I2Fe72w";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/vRR63CN.jpeg,https://i.imgur.com/ovCfp9J.jpeg,https://i.imgur.com/edzo5dj.jpeg";
 global.devs = "https://wa.me/50931461936 , https://wa.me/50931461936";
-global.sudo = process.env.SUDO || "50931461936";
-global.owner = process.env.OWNER_NUMBER || "PUT YOUR OWNER NUMBER";
+global.sudo = process.env.SUDO || "2349165278728";
+global.owner = process.env.OWNER_NUMBER || "2349165278728";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "true";
@@ -43,7 +43,7 @@ global.scan = "https://drk-tech-2.onrender.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "mettre votre session ici"
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMElGOTRkbml5aDgwRlgyUXAyWnBad2xhSnlvZksxY21sZXI3UEJjQVRtND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYyszZkwwOThFS0NSdXVYcVlORjJiSGx4bk4yM0VHVk5DZXY1RmxGMFNTYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwQ3F2SVBVY0EyKy9sQmVOZTljbEtzakNmdDE1SE5YZjBCS2FhQzV1bkcwPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJIemlxN2lUSXJWNkpkdUtNR21HK1RrcEZDb3BoQ3dhZ1FHSzFJNlVRRDEwPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdCRGRoelZaeG9xNWg1dXliUmpLTkFDRTAxL2szTlc1aXVUUXlGU2NTRW89In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjlBWVdSWisyN0NsS2o3S3Q0dm9sTTVyRmJURmZ1dFJlNGJLQXptNnVmbEk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid0pJRXdOQ1plY2tvaGo5MDUvQVJ1WldYVkZUOWJHVjlaTE9hZUJqV2pYaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK2dRMFg4MmZWU0VYK3VVNjllaDA2MWJGOFJoN3VNYmFQdFlHczJRK1dXST0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjMxRUpGWFFYYVJvckJRbm1WOHlZekE2WElQZWNjTWt6ZzVqT2dhSEJMVUFYZVhCbXNvaVB3VGtvNkpFSzNsejN2eVk5Z3FVYnlxWXdRS2hJUnBLUUJnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTY3LCJhZHZTZWNyZXRLZXkiOiJOdWxDUmVqUEZsdGNUb3VwL0Uyb2s2SGpGbjNYQU8ySE14cTlHSjVDKzdvPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJpdV93YzZKRlRPU1Y1X1NOM0NRTEFnIiwicGhvbmVJZCI6IjUzYmM4ODI4LTJhNTYtNDU1OS04YWEyLTJjNDRlYzZhNDNkZCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI5dFRSU25JYUExbU1lRHZGak8rR05WTkpEV1E9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY05LYm9wc3NiODRHRlFrKzBuamxER2pUR0F3PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IldYQURDMUw4IiwibWUiOnsiaWQiOiIyMzQ5MTY1Mjc4NzI4OjIwQHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKSFg2OGtDRUxiTTlid0dHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiI1TVhjcFFQUnlUREp5Y09iQno1RmpVckh2ODk2dk1SMUozUkNmbUM3M24wPSIsImFjY291bnRTaWduYXR1cmUiOiJoMytMQitZMWFiM2NKSHpXanlpRkRiS3ZIY2ZoeXd0ZHR1RURPQ2VxYW9hRUZwbzJOdmRyRVBqUVVWQUpVQUlHZWJVSU5nQ0tRQ1JycjJFandUU25EZz09IiwiZGV2aWNlU2lnbmF0dXJlIjoib2trZWp4NkF2eEhqQTBLb2ViYW9Nem54OFhJbHlGeVZiUXhKaTB4MHFPR3M0N2ljRW5YQmNzazRWYnVmajBzcHNUUXJHV3NWK1NQSzRSZkFobXdYQ1E9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzQ5MTY1Mjc4NzI4OjIwQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmVURjNLVUQwY2t3eWNuRG13YytSWTFLeDcvUGVyekVkU2QwUW41Z3U5NTkifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MzgzNjg1Nzl9"
 module.exports = {
   menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || ".",
@@ -64,7 +64,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "...d336",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
   LANG: (process.env.THEME || "STEN").toUpperCase(),
 };
 global.rank = "updated";
